cf85be737092aec074ae2d351102def087b9c270
Richard Silva <rdsilva_richard@hotmail.com>
2019-04-14 13:36:36 -0300

    gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require("gulp");
 const stylus = require("gulp-stylus");
 const pug = require("gulp-pug");
-const browserify = require("gulp-browserify");
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
 const concat = require("gulp-concat");
 const baseDir = "./public";
 
@@ -9,6 +12,7 @@ gulp.task("page", function buildHTML() {
   return gulp
     .src("./view/index.pug")
     .pipe(pug())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(baseDir));
 });
 
@@ -16,7 +20,12 @@ gulp.task("script", function() {
   return gulp
     .src(["./assets/scripts/*.js"])
     .pipe(concat("main.js"))
-    .pipe(browserify())
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())    
     .pipe(gulp.dest(baseDir + "/javascripts"));
 });
 
@@ -24,10 +33,8 @@ gulp.task("style", function() {
   return gulp
     .src("./assets/style/style.styl")
     .pipe(stylus())
+    .pipe(cleanCSS({compatibility: 'ie11'}))
     .pipe(gulp.dest(baseDir + "/stylesheets"));
 });
 
-gulp.task(
-  "default",
-  gulp.parallel("page", "script", "style")
-);
+gulp.task("default", gulp.parallel("page", "script", "style"));
