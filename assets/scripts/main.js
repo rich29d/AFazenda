b8d5a82185afd2a2622286c22e19874fdc983ecd
Richard Silva <rdsilva_richard@hotmail.com>
2019-04-14 16:34:49 -0300

    for in no lugar de for of

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,35 +5,35 @@ function loadListPerson() {
 
     if (Array.isArray(resp.data) && resp.data.length > 0) {
       resp.data.sort((p1, p2) => {
-        if (p1.positive > p2.positive)
-          return -1
-  
-        if (p1.positive < p2.positive)
-          return 1
-  
-        return 0
-      })
-  
-      for (const person of resp.data) {
+        if (p1.positive > p2.positive) return -1;
+
+        if (p1.positive < p2.positive) return 1;
+
+        return 0;
+      });
+
+      for (const i in resp.data) {
+        const person = resp.data[i];
+
         maxPositive += +person.positive || 0;
         maxNegative += +person.negative || 0;
       }
-  
+
       for (const i in resp.data) {
         const person = resp.data[i];
-  
+
         person.percPositive = getPercentage(person.positive, maxPositive);
         person.percNegative = getPercentage(person.negative, maxNegative);
         person.position = +i + 1;
-  
+
         append(".List__Body", template.person(person));
       }
     } else {
       append(".List__Body", template.empty());
     }
 
-    const listEl = document.querySelector('.List__Body');
-    listEl.style.display = 'inline-block';
+    const listEl = document.querySelector(".List__Body");
+    listEl.style.display = "inline-block";
   });
 }
 
